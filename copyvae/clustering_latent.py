e0472ea8f04be02d78330a3e15be41be8a1492d5
josebonet <jose.bonet@irbbarcelona.org>
2021-07-19 11:35:57 +0200

    include GMM for clustering

--- copyvae/clustering_latent.py
+++ copyvae/clustering_latent.py
@@ -4,10 +4,16 @@ import os
 import click
 import numpy as np
 import pandas as pd
+import scanpy as sc
 import seaborn as sns
 from scipy import stats
 import matplotlib.pylab as plt
 from collections import Counter
+from matplotlib.patches import Ellipse
+
+from sklearn.metrics import accuracy_score
+from sklearn.mixture import GaussianMixture
+from sklearn.metrics.cluster import v_measure_score
 
 
 def _cluster_with_kmeans(data_in, k=None, metric='euclidean'):
@@ -28,15 +34,15 @@ def _cluster_with_kmeans(data_in, k=None, metric='euclidean'):
     data[np.isnan(data)] = 3
 
     if not k:
-        for k in range(2, 5, 1):
-            clustering = KMeans(n_clusters=k, tol=1e-6, n_init=500).fit(data)
+        for k in range(2, 10, 1):
+            clustering = KMeans(n_clusters=k, n_init=100).fit(data)
             score = metrics.silhouette_score(
                 data, clustering.labels_, metric=metric
             )
             cluster_val.append((score, clustering))
 
     else:
-        clustering = KMeans(n_clusters=k, tol=1e-6, n_init=500).fit(data)
+        clustering = KMeans(n_clusters=k, n_init=100).fit(data)
         score = metrics.silhouette_score(
             data, clustering.labels_, metric=metric
         )
@@ -51,26 +57,96 @@ def _cluster_with_kmeans(data_in, k=None, metric='euclidean'):
     return [i[0] for i in cell_clusters], best_clustering.labels_
 
 
+def draw_umap(adata, feature, file_name):
+
+    sc.pp.neighbors(adata, use_rep=feature)
+    sc.tl.umap(adata, min_dist=0.3)
+    sc.pl.umap(
+                adata,
+                color=["cell_type"],
+                frameon=False,
+                save='{}.png'.format(file_name)
+                )
+
+    return None
+    
+
 @click.command(short_help='script for clustering the latent space')
 @click.option(
     '-da', '--data', default='', help='copy number data'
 )
 @click.option(
+    '-dk', '--data_copykat', default='', help='Results form copykat'
+)
+@click.option(
     '-o', '--output', default='', help='output folder'
 )
-def main(data, output):
+def main(data, data_copykat, output):
     # from julia.api import Julia
     # jl = Julia(compiled_modules=False)
     # from dpmmpython.priors import niw
     # from dpmmpython.dpmmwrapper import DPMMPython
-    
-    X = np.load(data)
-    # prior = niw(1, np.zeros(10), 20, np.eye(10))
-    
     # labels, clusters ,sub_labels= DPMMPython.fit(
     #     X.T, 10000, prior = prior, verbose=False)
-    aa, labels = _cluster_with_kmeans(X)
+    
+    X = np.load(data)
+    normal = np.ones(379)
+    tumor = np.zeros(1480 - 379)
+    gt = np.concatenate([normal, tumor])
+
+    tumor_data = X[379:]
+
+    if data_copykat:
+        copykat = pd.read_csv(data_copykat, sep='\t')
+        copykat_pred = copykat.loc['copykat.pred'].values
+        cluster_pred = copykat.loc['cluster.pred'].values
+
+        acc_ck = accuracy_score (copykat_pred, cluster_pred)
+        v_mes_ck = v_measure_score(copykat_pred, cluster_pred)
+        # import pdb;pdb.set_trace()
+
+    # prior = niw(1, np.zeros(10), 20, np.eye(10))
+    
+    
+    # aa, labels = _cluster_with_kmeans(X)
+    # aa_2, labels_2 = _cluster_with_kmeans(X, k=2)
+
+
+    # gaussian mixture clustering
+    # define the model
+    model = GaussianMixture(n_components=2, n_init=5, max_iter=100000)
+    # fit the model
+    model.fit(X)
+    # assign a cluster to each example
+    yhat = model.predict(X)
+    acc_vae = accuracy_score (yhat, gt)
+    v_mes_vae = v_measure_score(yhat, gt)
+    print(acc_ck, v_mes_ck)
+    print(acc_vae, v_mes_vae)
+
+    import pdb;pdb.set_trace()  
+
+    tumor_cells = X[np.argwhere(yhat == 0).flatten()]
+    normal_cells = X[np.argwhere(yhat == 1).flatten()]
+    print(tumor_cells.shape, normal_cells.shape)
+    
+    n_components = np.arange(1,10)
+    models = [GaussianMixture(n, covariance_type='full', random_state=0, 
+        n_init=5, max_iter=1000).fit(tumor_cells) for n in n_components]
+
+    plt.plot(n_components, [m.bic(X) for m in models], label='BIC')
+    plt.legend(loc='best')
+    plt.xlabel('n_components');
+    plt.show()
     import pdb;pdb.set_trace()
+
+    models_normal = [GaussianMixture(n, covariance_type='full', random_state=0, 
+        n_init=5, max_iter=1000).fit(normal_cells) for n in n_components]
+
+    plt.plot(n_components, [m.bic(X) for m in models_normal], label='BIC')
+    plt.legend(loc='best')
+    plt.xlabel('n_components');
+    plt.show()
 
 if __name__ == '__main__':
     main()
